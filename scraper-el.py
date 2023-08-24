025b2413d8995c0034e2bd79a8e84f684f8a5dda
Ethan Lee <ethanlee.dev1@gmail.com>
2023-08-24 10:50:28 -0400

    use pandas to read the csv file

--- scraper-el.py
+++ scraper-el.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import unittest
+import pandas as pd
 
 # AmeriSave Mortgage
 
@@ -13,33 +14,34 @@ date = []
 comments = []
 stars = []
 
-for i in range(1, 30):
-    page_to_scrape = requests.get(
-        f"https://www.consumeraffairs.com/finance/amerisave_mortgage.html?page={i}#scroll_to_reviews=true"
-    )
-    soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-    reviews = soup.findAll("div", attrs={"class": "rvw__cntr"})
 
-    for review in reviews:
-        authors.append(review.find("span", attrs={"class": "rvw__inf-nm"}).text)
-        location.append(review.find("span", attrs={"class": "rvw__inf-lctn"}).text)
-        date.append(review.find("span", attrs={"class": "rvw__rvd-dt"}).text)
-        comments.append(review.find("p").text)
-        star = review.find("meta", attrs={"itemprop": "ratingValue"})
-        if star:
-            stars.append(star.get("content"))
-        else:
-            stars.append("N/A")
+def create_csv():
+    for i in range(1, 30):
+        page_to_scrape = requests.get(
+            f"https://www.consumeraffairs.com/finance/amerisave_mortgage.html?page={i}#scroll_to_reviews=true"
+        )
+        soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+        reviews = soup.findAll("div", attrs={"class": "rvw__cntr"})
 
+        for review in reviews:
+            authors.append(review.find("span", attrs={"class": "rvw__inf-nm"}).text)
+            location.append(review.find("span", attrs={"class": "rvw__inf-lctn"}).text)
+            date.append(review.find("span", attrs={"class": "rvw__rvd-dt"}).text)
+            comments.append(review.find("p").text)
+            star = review.find("meta", attrs={"itemprop": "ratingValue"})
+            if star:
+                stars.append(star.get("content"))
+            else:
+                stars.append("N/A")
 
-combined_data = zip(authors, location, date, comments, stars)
+    combined_data = zip(authors, location, date, comments, stars)
 
-with open(csv_file_path, mode="w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(
-        ["Author", "Location", "Date", "Comments", "Rating"]
-    )  # Write header row
-    writer.writerows(combined_data)
+    with open(csv_file_path, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            ["Author", "Location", "Date", "Comments", "Rating"]
+        )  # Write header row
+        writer.writerows(combined_data)
 
 
 class TestScraping(unittest.TestCase):
@@ -49,4 +51,7 @@ class TestScraping(unittest.TestCase):
             self.assertTrue(len(lines) > 0)
 
 
-print(unittest.main())
+df = pd.read_csv(csv_file_path, encoding="ISO-8859-1")
+
+print("pandas reading file: ", df.head())
+print("unit testing: ", unittest.main())
